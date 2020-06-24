e1261c54ad7f169a46cbcb3d561182a247813fbd
DiegoRafaelAF <draf0314@gmail.com>
2020-06-24 18:18:00 -0300

    Codeclimate 2

--- src/App.js
+++ src/App.js
@@ -21,22 +21,22 @@ import './App.css';
 function App() {
   return (
     <Switch>
-      <Route exact path={"/"} component={LoginScreen} />
-      <Route exact path={"/comidas"} component={MainFoodScreen} />
-      <Route exact path={"/bebidas"} component={MainDrinkScreen} />
+      <Route exact path={'/'} component={LoginScreen} />
+      <Route exact path={'/comidas'} component={MainFoodScreen} />
+      <Route exact path={'/bebidas'} component={MainDrinkScreen} />
       <Route exact path={`/comidas/${'id-da-receita'}`} component={DetailsFoodScreen} />
       <Route exact path={`/bebidas/${'id-da-receita'}`} component={DetailsDrinkScreen} />
       <Route exact path={`/comidas/${'id-da-receita'}/in-progress`} component={ProgressFoodScreen} />
       <Route exact path={`/bebidas/${'id-da-receita'}/in-progress`} component={ProgressDrinkScreen} />
-      <Route exact path={"/explorar"} component={ExploreHomeScreen} />
-      <Route exact path={"/explorar/comidas"} component={ExploreFoodScreen} />
-      <Route exact path={"/explorar/bebidas"} component={ExploreDrinkScreen} />
-      <Route exact path={"/explorar/comidas/ingredientes"} component={ExploreFoodIngredientScreen} />
-      <Route exact path={"/explorar/bebidas/ingredientes"} component={ExploreDrinkIngredientScreen} />
-      <Route exact path={"/explorar/comidas/area"} component={ExploreFoodOriginScreen} />
-      <Route exact path={"/perfil"} component={ProfileScreen} />
-      <Route exact path={"/receitas-feitas"} component={DoneRecipes} />
-      <Route exact path={"/receitas-favoritas"} component={FavoriteRecipes} />
+      <Route exact path={'/explorar'} component={ExploreHomeScreen} />
+      <Route exact path={'/explorar/comidas'} component={ExploreFoodScreen} />
+      <Route exact path={'/explorar/bebidas'} component={ExploreDrinkScreen} />
+      <Route exact path={'/explorar/comidas/ingredientes'} component={ExploreFoodIngredientScreen} />
+      <Route exact path={'/explorar/bebidas/ingredientes'} component={ExploreDrinkIngredientScreen} />
+      <Route exact path={'/explorar/comidas/area'} component={ExploreFoodOriginScreen} />
+      <Route exact path={'/perfil'} component={ProfileScreen} />
+      <Route exact path={'/receitas-feitas'} component={DoneRecipes} />
+      <Route exact path={'/receitas-favoritas'} component={FavoriteRecipes} />
     </Switch>
   );
 }
--- src/components/DetailsDrinkScreen.jsx
+++ src/components/DetailsDrinkScreen.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function DetailsDrinkScreen() {
   return (
     <div>
-     <h1>DetailsDrinkScreen</h1>
+      <h1>DetailsDrinkScreen</h1>
     </div>
   );
 }
