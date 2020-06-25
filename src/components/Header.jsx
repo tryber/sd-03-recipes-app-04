36ccaabc261c624ea38a9e7ab3c4eca054b19e7f
cpereira <cpereiramt@gmail.com>
2020-06-25 16:47:33 -0400

    Salvando alterações no header do projeto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <div>
+    <div className="headerDiv">
       <i className="userProfileIcon" data-testeid="profile-top-btn" />
       <h1 className="foodTitle" data-testid="page-title">Comidas</h1>
       <i className="searchIcon" data-testid="search-top-btn" />
