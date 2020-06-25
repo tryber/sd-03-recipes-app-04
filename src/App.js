aeff07136a7fdcfb43a2ff4964c089c463d6a2c6
DiegoRafaelAF <draf0314@gmail.com>
2020-06-25 14:07:12 -0300

    Login page

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import DetailsDrinkScreen from './components/DetailsDrinkScreen';
-import DetailsFoodScreen from './components/DetailsFoodScreen';
-import DoneRecipes from './components';
-import ExploreDrinkIngredientScreen from './components/ExploreDrinkIngredientScreen';
-import ExploreDrinkScreen from './components/ExploreDrinkScreen';
-import ExploreFoodIngredientScreen from './components/ExploreFoodIngredientScreen';
-import ExploreFoodOriginScreen from './components/ExploreFoodOriginScreen';
-import ExploreFoodScreen from './components/ExploreFoodScreen';
-import ExploreHomeScreen from './components/ExploreHomeScreen';
-import FavoriteRecipes from './components/FavoriteRecipes';
+// import DetailsDrinkScreen from './components/ExploreDrinkScreen';
+// import DetailsFoodScreen from './components/DetailsFoodScreen';
+// import DoneRecipes from './components';
+// import ExploreDrinkIngredientScreen from './components/ExploreDrinkIngredientScreen';
+// import ExploreDrinkScreen from './components/ExploreDrinkScreen';
+// import ExploreFoodIngredientScreen from './components/ExploreFoodIngredientScreen';
+// import ExploreFoodOriginScreen from './components/ExploreFoodOriginScreen';
+// import ExploreFoodScreen from './components/ExploreFoodScreen';
+// import ExploreHomeScreen from './components/ExploreHomeScreen';
+// import FavoriteRecipes from './components/FavoriteRecipes';
 import LoginScreen from './components/LoginScreen';
-import MainDrinkScreen from './components/MainDrinkScreen';
-import MainFoodScreen from './components/MainFoodScreen';
-import ProfileScreen from './components/ProfileScreen';
-import ProgressDrinkScreen from './components/ProgressDrinkScreen';
-import ProgressFoodScreen from './components/ProgressFoodScreen';
-import './App.css';
+// import MainDrinkScreen from './components/MainDrinkScreen';
+// import MainFoodScreen from './components/MainFoodScreen';
+// import ProfileScreen from './components/ProfileScreen';
+// import ProgressDrinkScreen from './components/ProgressDrinkScreen';
+// import ProgressFoodScreen from './components/ProgressFoodScreen';
+// import './App.css';
+
+import AplicationProvider from './context/ContextAplication';
 
 function App() {
   return (
-    // <Switch>
-    //   <Route exact path={'/'} component={LoginScreen} />
-    //   <Route exact path={'/comidas'} component={MainFoodScreen} />
-    //   <Route exact path={'/bebidas'} component={MainDrinkScreen} />
-    //   <Route exact path={`/comidas/${'id-da-receita'}`} component={DetailsFoodScreen} />
-    //   <Route exact path={`/bebidas/${'id-da-receita'}`} component={DetailsDrinkScreen} />
-    //   <Route exact path={`/comidas/${'id-da-receita'}/in-progress`} component={ProgressFoodScreen} />
-    //   <Route exact path={`/bebidas/${'id-da-receita'}/in-progress`} component={ProgressDrinkScreen} />
-    //   <Route exact path={'/explorar'} component={ExploreHomeScreen} />
-    //   <Route exact path={'/explorar/comidas'} component={ExploreFoodScreen} />
-    //   <Route exact path={'/explorar/bebidas'} component={ExploreDrinkScreen} />
-    //   <Route exact path={'/explorar/comidas/ingredientes'} component={ExploreFoodIngredientScreen} />
-    //   <Route exact path={'/explorar/bebidas/ingredientes'} component={ExploreDrinkIngredientScreen} />
-    //   <Route exact path={'/explorar/comidas/area'} component={ExploreFoodOriginScreen} />
-    //   <Route exact path={'/perfil'} component={ProfileScreen} />
-    //   <Route exact path={'/receitas-feitas'} component={DoneRecipes} />
-    //   <Route exact path={'/receitas-favoritas'} component={FavoriteRecipes} />
-    // </Switch>
-    <div>Heloo</div>
+    <AplicationProvider>
+      <Switch>
+        <Route exact path={'/'} component={LoginScreen} />
+        {/* <Route exact path={'/comidas'} component={MainFoodScreen} />
+        <Route exact path={'/bebidas'} component={MainDrinkScreen} />
+        <Route exact path={`/comidas/${'id-da-receita'}`} component={DetailsFoodScreen} />
+        <Route exact path={`/bebidas/${'id-da-receita'}`} component={DetailsDrinkScreen} />
+        <Route exact path={`/comidas/${'id-da-receita'}/in-progress`} component={ProgressFoodScreen} />
+        <Route exact path={`/bebidas/${'id-da-receita'}/in-progress`} component={ProgressDrinkScreen} />
+        <Route exact path={'/explorar'} component={ExploreHomeScreen} />
+        <Route exact path={'/explorar/comidas'} component={ExploreFoodScreen} />
+        <Route exact path={'/explorar/bebidas'} component={ExploreDrinkScreen} />
+        <Route exact path={'/explorar/comidas/ingredientes'} component={ExploreFoodIngredientScreen} />
+        <Route exact path={'/explorar/bebidas/ingredientes'} component={ExploreDrinkIngredientScreen} />
+        <Route exact path={'/explorar/comidas/area'} component={ExploreFoodOriginScreen} />
+        <Route exact path={'/perfil'} component={ProfileScreen} />
+        <Route exact path={'/receitas-feitas'} component={DoneRecipes} />
+        <Route exact path={'/receitas-favoritas'} component={FavoriteRecipes} /> */}
+      </Switch>
+    </AplicationProvider>
   );
 }
 
--- src/components/LoginScreen.jsx
+++ src/components/LoginScreen.jsx
@@ -1,66 +1,56 @@
 import React, { useContext, useState } from 'react';
+import { ContextAplication } from '../context/ContextAplication';
 
-const { saveInput } = useContext();
-const [input, setInput] = useState({ email: '', password: '' });
-const [informations, setInformations] = useState(false);
-
-// const saveInput = (input) => {
-//   const inputsLogin = {
-//     email: input.email,
-//     password: input.password,
-//   };
-//   setInput(inputsLogin);
-// };
-
-const IsEmail = (name, email) => {
-  const exclude = /[^@-.w]|^[_@.-]|[._-]{2}|[@.]{2}|(@)[^@]*1/;
-  const check = /@[w-]+./;
-  const checkend = /.[a-zA-Z]{2,3}$/;
+const IsEmail = (name, email, input, setInput, setInformations) => {
+  const exclude = '/[^@-.w]|^[_@.-]|[._-]{2}|[@.]{2}|(@)[^@]*1/';
+  const check = '/@[w-]+./';
+  const checkend = '/.[a-zA-Z]{2,3}$/';
   if (
-    email.search(exclude) !== -1 ||
-    email.search(check) === -1 ||
-    email.search(checkend) === -1
+    email.search(exclude) !== -1
+    || email.search(check) === -1
+    || email.search(checkend) === -1
   ) {
     return setInput({ ...input, [name]: email }) && setInformations(false);
   }
   return setInput({ ...input, [name]: email }) && setInformations(true);
 };
 
-const IsPassword = (name, password) => {
+const IsPassword = (name, password, input, setInput, setInformations) => {
   setInput({ ...input, [name]: password });
   if (input.password.length >= 6) return setInformations(true);
   return null;
-}
+};
 
-const handleChangeInput = (name, ele) => {
-  if (name === 'email') return IsEmail(name, ele)
+const handleChangeInput = (name, ele, input, setInput) => {
+  if (name === 'email') return IsEmail(name, ele, input, setInput);
   return IsPassword(name, ele);
 };
 
-const handleFormSubmit = () => {
+const handleFormSubmit = (saveInput, input) => {
   saveInput(input);
 };
 
-const createForm = () => {
+const createForm = (saveInput, input, setInput, informations, setInformations) => {
   return (
     <form>
       <h2>Login</h2>
       <input
         data-testid="email-input"
-        onChange={(ele) => handleChangeInput('email', ele.target.value)}
+        onChange={(ele) => handleChangeInput('email', ele.target.value, input, setInput, setInformations)}
         type="text"
         name="email"
       />
       <input
         data-testid="password-input"
-        onChange={(ele) => handleChangeInput('password', ele.target.value)}
+        onChange={(ele) => handleChangeInput('password', ele.target.value, input, setInput, setInformations)}
         type="text"
         name="password"
       />
       <button
+        type="button"
         disabled={informations}
         data-testid="login-submit-btn"
-        onClick={() => handleFormSubmit}
+        onClick={handleFormSubmit(saveInput, input)}
       >
         Entrar
       </button>
@@ -69,7 +59,15 @@ const createForm = () => {
 };
 
 function LoginScreen() {
-  return <div>{createForm}</div>;
+  const { saveInput } = useContext(ContextAplication);
+  const [input, setInput] = useState({ email: '', password: '' });
+  const [informations, setInformations] = useState(false);
+
+  return (
+    <div>
+      {createForm([saveInput, input, setInput, informations, setInformations])}
+    </div>
+  );
 }
 
 export default LoginScreen;
