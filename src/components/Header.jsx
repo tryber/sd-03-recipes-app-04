05395e2ed587a231c39f06457f97039f0fe1d808
cpereira <cpereiramt@gmail.com>
2020-06-25 20:43:47 -0400

    Salvando alterações no Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
+import './Header.css';
 
 function Header() {
+  const [isInputSearchVisible, setIsInputSearchVisible] = useState(false);
+
+  function showHideInputSearch() {
+    if (isInputSearchVisible) {
+      setIsInputSearchVisible(false);
+    } else {
+      setIsInputSearchVisible(true);
+    }
+  }
   return (
     <div className="headerDiv">
-      <i className="userProfileIcon" data-testeid="profile-top-btn" />
+      <img className="userProfileIcon" src={profileIcon} alt="ProfileIcon" data-testeid="profile-top-btn" />
       <h1 className="foodTitle" data-testid="page-title">Comidas</h1>
-      <i className="searchIcon" data-testid="search-top-btn" />
+      {isInputSearchVisible
+        ? (
+          <input
+            type="text"
+            name="search-Input"
+            placeholder="Typing for search"
+          />
+        )
+        : null}
+
+      <img
+        src={searchIcon}
+        alt="SearchIcon"
+        className="searchIcon"
+        data-testid="search-top-btn"
+      />
     </div>
   );
 }
--- src/components/MainDrinkScreen.jsx
+++ src/components/MainDrinkScreen.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 function MainDrinkScreen() {
   return (
     <div>
+      <Header />
       <h1>MainDrinkScreen</h1>
     </div>
   );
