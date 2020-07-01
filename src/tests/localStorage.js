204e4eb5da7cb7ea538f4725f81499aaaeb63ef0
DiegoRafaelAF <draf0314@gmail.com>
2020-07-01 18:46:17 -0300

    codeclimate 2

--- src/tests/localStorage.js
+++
@@ -1,7 +0,0 @@
-export default class LocalStorage {
-  constructor(obj = {}) {this.store = obj; };
-  setItem = (key, val) => { this.store[key] = val.toString(); };
-  getItem = key => this.store[key];
-  removeItem = key => { delete this.store[key]; };
-  clear = () => { this.store = {} };
-}();
