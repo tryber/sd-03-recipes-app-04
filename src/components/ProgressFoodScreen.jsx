2a9298f48d90097cd8e18aea9d0766725b147474
clayton Pereira <cpereiramt@gmail.com>
2020-06-30 23:56:18 -0300

    changing recipes in progress screen

--- src/components/ProgressFoodScreen.jsx
+++ src/components/ProgressFoodScreen.jsx
@@ -1,29 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getMealById } from '../services/foodApi';
 
 function ProgressFoodScreen(props) {
+  const [inProgressRecipe, setInProgressRecipe] = useState([]);
   const { id } = props.match.params;
   useEffect(() => {
     getMealById(id).then((data) => {
-      console.log(data.meals);
+      setInProgressRecipe(data.meals[0]);
+      console.log(data.meals[0]);
     });
-  });
-  console.log(id);
+  }, []);
   return (
     <div>
-      InProgressRecipe Placeholder
-      <img src="" alt="" data-testid="recipe-photo" />
+      <img src={inProgressRecipe.strMealThumb} alt="" data-testid="recipe-photo" />
       <button type="button" data-testid="share-btn"> share Button </button>
       <button type="button" data-testid="favorite-btn"> Favorite Button</button>
-      <h1 data-testid="recipe-title">Recipe Title Placeholder </h1>
-      <h3 data-testid="recipe-category">Recipe Category Placeholder</h3>
+      <h1 data-testid="recipe-title">
+        {inProgressRecipe.strMeal}
+      </h1>
+      <h3 data-testid="recipe-category">{inProgressRecipe.strCategory}</h3>
       <ul>
         <li>
           Recipe List Placeholder
         </li>
       </ul>
       <div data-testid="instructions">
-        Placeholder Instructions
+        {inProgressRecipe.strInstructions}
       </div>
       <button data-testid="finish-recipe-btn" type="button">
         Finish Recipe Button
