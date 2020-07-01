42140cb90be4fca41e0fc431496704f00aa0ca57
clayton Pereira <cpereiramt@gmail.com>
2020-06-30 22:36:09 -0300

    Starting recipes in progress Screen

--- src/components/ProgressFoodScreen.jsx
+++ src/components/ProgressFoodScreen.jsx
@@ -3,7 +3,23 @@ import React from 'react';
 function ProgressFoodScreen() {
   return (
     <div>
-      <h1>ProgressFoodScreen</h1>
+      InProgressRecipe Placeholder
+      <img src="" alt="" data-testid="recipe-photo" />
+      <button type="button" data-testid="share-btn"> share Button </button>
+      <button type="button" data-testid="favorite-btn"> Favorite Button</button>
+      <h1 data-testid="recipe-title">Recipe Title Placeholder </h1>
+      <h3 data-testid="recipe-category">Recipe Category Placeholder</h3>
+      <ul>
+        <li>
+          Recipe List Placeholder
+        </li>
+      </ul>
+      <div data-testid="instructions">
+        Placeholder Instructions
+      </div>
+      <button data-testid="finish-recipe-btn" type="button">
+        Finish Recipe Button
+      </button>
     </div>
   );
 }
